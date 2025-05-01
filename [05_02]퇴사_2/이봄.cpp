539d151685c7a449ea091ead71e98e5ce6679c0f
leebohm <102368451+leebohm@users.noreply.github.com>
2025-05-01 21:59:42 +0900

    Update 이봄.cpp

--- [05_02]퇴사_2/이봄.cpp
+++ [05_02]퇴사_2/이봄.cpp
@@ -18,12 +18,16 @@ int main() {
 
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i][0] >> arr[i][1];
+		// 소요 기간 // 금액 
 	}
 	
 	for (int i = 1; i <= n; i++) {
+		// i일에 시작하면 d일에 상담 끝남
 		int d = arr[i][0] + i-1;
 		dp[i] = max(dp[i], dp[i - 1]);
+		// i일에 아무 상담도 하지 않은 경우 
 		if (d <= n + 1) {
+			// i일에 상담을 하고 d일에 끝났을 때 업데이트 
 			dp[d] = max(dp[i - 1] + arr[i][1], dp[d]);
 		}
 
