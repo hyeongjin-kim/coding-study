2f9cfe3459556278c30d4f74fb5b6e55d09a752d
leebohm <102368451+leebohm@users.noreply.github.com>
2025-05-06 20:28:10 +0900

    Update 이봄.cpp

--- [05_07]카드_정렬하기/이봄.cpp
+++ [05_07]카드_정렬하기/이봄.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+#define MAX_N 100005
+
+int n;
+int card_size[MAX_N];
+priority_queue<int,vector<int>, greater<>> pq;
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> n;
+    
+    for(int i=0; i<n; i++){
+        int tmp;
+        cin >> tmp;
+        pq.push(tmp);
+    }
+
+    int ans = 0;
+    
+    while(pq.empty() == false){
+        int a = pq.top();
+        pq.pop();
+        if(pq.empty() == true)
+            break;
+        int b = pq.top();
+        pq.pop();
+        //cout << a <<" " << b <<"\n";
+        ans += (a+b);
+        pq.push(a+b);
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
 
