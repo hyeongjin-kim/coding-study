d4c2da1abee895edb6c4aa24972f94f2e58cbd7e
jin0410 <64190888+jin0410@users.noreply.github.com>
2025-05-11 23:17:56 +0900

    파로마!

---
+++ [05_12]벽장문의_이동/강진규.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+struct STEP {
+	int left, right, idx, step;
+
+	bool operator<(const STEP& r) const {
+		if (left != r.left) return left < r.left;
+		if (right != r.right) return right < r.right;
+		return idx < r.idx;
+	}
+};
+
+int main(void) {
+	int n, m;
+	int use[20];
+	cin >> n;
+	int left, right;
+	cin >> left >> right;
+	if (left > right)
+		swap(left, right);
+	cin >> m;
+	for (int i = 0;i < m;++i)
+		cin >> use[i];
+	
+	queue<STEP> q;
+	set<STEP> vis;
+	q.push({ left, right, 0, 0 });
+	vis.insert({ left, right, 0, 0 });
+	while (!q.empty()) {
+		STEP cur = q.front(); q.pop();
+		int curLeft = cur.left, curRight = cur.right;
+		int curIdx = cur.idx, curStep = cur.step;
+		if (curIdx >= m) {
+			cout << curStep;
+			return 0;
+		}
+		if (use[curIdx] == curLeft || use[curIdx] == curRight) {
+			++curIdx;
+			if (vis.find({ curLeft, curRight, curIdx, 0 }) != vis.end()) continue;
+			vis.insert({ curLeft, curRight, curIdx, 0 });
+			q.push({ curLeft,curRight,curIdx,curStep });
+			continue;
+		}
+		if (use[curIdx] < curLeft) {
+			int nextLeft = curLeft - 1;
+			if (nextLeft < 1) continue;
+			if (vis.find({ nextLeft, curRight, curIdx, 0 }) != vis.end()) continue;
+			vis.insert({ nextLeft, curRight, curIdx, 0 });
+			q.push({ nextLeft, curRight, curIdx, curStep + 1 });
+		}
+		else if (use[curIdx] > curLeft && use[curIdx] < curRight) {
+			int nextLeft = curLeft + 1;
+			int nextRight = curRight - 1;
+			if (vis.find({ nextLeft, curRight, curIdx, 0 }) == vis.end()) {
+				vis.insert({ nextLeft, curRight, curIdx, 0 });
+				q.push({ nextLeft, curRight, curIdx, curStep + 1 });
+			}
+
+			if (vis.find({ curLeft, nextRight, curIdx, 0 }) == vis.end()) {
+				vis.insert({ curLeft, nextRight, curIdx, 0 });
+				q.push({ curLeft, nextRight, curIdx, curStep + 1 });
+			}
+		}
+		else {
+			int nextRight = curRight + 1;
+			if (nextRight > n) continue;
+			if (vis.find({ curLeft, nextRight, curIdx, 0 }) != vis.end()) continue;
+			vis.insert({ curLeft, nextRight, curIdx, 0 });
+			q.push({ curLeft, nextRight, curIdx, curStep + 1 });
+		}
+	}
+	return 0;
+}
